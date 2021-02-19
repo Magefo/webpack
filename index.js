bd7127137986e0fe8c09f679914681ba3d46760f
Miguel <miguel.ingsistemas@gmail.com>
2021-02-18 19:09:24 -0500

    Adding the first cdn library

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var nameInputEl = document.getElementById('name-input');
 var nameOutputEl = document.getElementById('name-output');
 
 nameInputEl.addEventListener('input', function(event) {
-    nameOutputEl.innerHTML = event.target.value;
+    var formatedDate = moment(event.target.value).format('MMMM Do YYYY');
+    nameOutputEl.innerHTML = formatedDate;
 })
