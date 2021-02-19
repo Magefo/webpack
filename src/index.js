1c2892f35c494e6345206b6ac961c37cd35e8182
Miguel <miguel.ingsistemas@gmail.com>
2021-02-19 07:30:00 -0500

    Add css loader and html webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import moment from 'moment';
+// import moment from 'moment';
+import './style.css';
 
 var nameInputEl = document.getElementById('name-input');
 var nameOutputEl = document.getElementById('name-output');
 
 nameInputEl.addEventListener('input', function(event) {
-    var formatedDate = moment(event.target.value).format('MMMM Do YYYY');
-    nameOutputEl.innerHTML = formatedDate;
+    // var formatedDate = moment(event.target.value).format('MMMM Do YYYY');
+    nameOutputEl.innerHTML = event.target.value; // formatedDate;
 });
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 4200
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
