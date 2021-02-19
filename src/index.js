fd627526be68ce8b6f2f1797e4d17ea26bc16488
Miguel <miguel.ingsistemas@gmail.com>
2021-02-18 19:45:57 -0500

    Add npm, moment and webpack packages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('hola');
+import moment from 'moment';
 
 var nameInputEl = document.getElementById('name-input');
 var nameOutputEl = document.getElementById('name-output');
@@ -6,4 +6,4 @@ var nameOutputEl = document.getElementById('name-output');
 nameInputEl.addEventListener('input', function(event) {
     var formatedDate = moment(event.target.value).format('MMMM Do YYYY');
     nameOutputEl.innerHTML = formatedDate;
-})
+});
