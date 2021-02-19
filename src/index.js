b1ab9a582dc83d86abc20855a8cfc399b888a38c
Miguel <miguel.ingsistemas@gmail.com>
2021-02-19 08:36:19 -0500

    Add async await support and browserlistrc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 // import moment from 'moment';
-import './style.css';
+import "./style.css";
 
-const nameInputEl = document.getElementById('name-input');
-const nameOutputEl = document.getElementById('name-output');
+const nameInputEl = document.getElementById("name-input");
+const nameOutputEl = document.getElementById("name-output");
 
-nameInputEl.addEventListener('input', event => 
+nameInputEl.addEventListener(
+  "input",
+  (event) =>
     // var formatedDate = moment(event.target.value).format('MMMM Do YYYY');
-    nameOutputEl.innerHTML = event.target.value // formatedDate;
+    (nameOutputEl.innerHTML = event.target.value) // formatedDate;
 );
+
+const delay = async (time) => await new Promise((resolve) => setTimeout(() => resolve(), time));
+
+delay(3000).then(() => alert('Hola Miguel'));
