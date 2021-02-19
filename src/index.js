c8051f9a1df26872189741f16bb302ccc95845f0
Miguel <miguel.ingsistemas@gmail.com>
2021-02-19 08:13:18 -0500

    Transform code and add babel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // import moment from 'moment';
 import './style.css';
 
-var nameInputEl = document.getElementById('name-input');
-var nameOutputEl = document.getElementById('name-output');
+const nameInputEl = document.getElementById('name-input');
+const nameOutputEl = document.getElementById('name-output');
 
-nameInputEl.addEventListener('input', function(event) {
+nameInputEl.addEventListener('input', event => 
     // var formatedDate = moment(event.target.value).format('MMMM Do YYYY');
-    nameOutputEl.innerHTML = event.target.value; // formatedDate;
-});
+    nameOutputEl.innerHTML = event.target.value // formatedDate;
+);
